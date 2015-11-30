2db99284482a84400884afccfbd47ac68866175b
tkrausjr <tkrausjr@gmail.com>
2015-11-30 10:03:13 -0500

    Changed input to raw_input for Python3

--- autoscale.py
+++ autoscale.py
@@ -5,12 +5,12 @@ import requests
 import json
 
 marathon_host = 'thomaskra-elasticl-1114imswizjvw-1820440244.us-east-1.elb.amazonaws.com'
-# marathon_host = input("Enter the resolvable hostname or IP of your Marathon Instance : ")
-marathon_app = input("Enter the Marathon Application Name to Configure Autoscale for from the Marathon UI : ")
-# max_mem_percent = input("Enter the Max percent of Mem Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : ")
-# max_cpu_percent = input("Enter the Max percent of CPU Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : ")
-# trigger_mode = input("Enter which metric(s) to trigger Autoscale (and / or) : ")
-# autoscale_multiplier = input("Enter Autoscale multiplier for triggered Autoscale (ie 1.5) : ")
+# marathon_host = raw_input("Enter the resolvable hostname or IP of your Marathon Instance : ")
+marathon_app = raw_input("Enter the Marathon Application Name to Configure Autoscale for from the Marathon UI : ")
+# max_mem_percent = raw_input("Enter the Max percent of Mem Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : ")
+# max_cpu_percent = raw_input("Enter the Max percent of CPU Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : ")
+# trigger_mode = raw_input("Enter which metric(s) to trigger Autoscale (and / or) : ")
+# autoscale_multiplier = raw_input("Enter Autoscale multiplier for triggered Autoscale (ie 1.5) : ")
 
 def get_all_apps(marathon_host):
     response=requests.get('http://' + marathon_host+ '/marathon/v2/apps').json()
