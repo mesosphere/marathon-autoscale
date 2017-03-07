6fd47c439f48e682c26ff9503cc88f587f596ab6
Jose Maria Alvarez <josemaria.alvarezfernandez@elcorteingles.es>
2017-03-07 15:56:46 +0100

    Add scale_down policy

--- marathon-autoscale.py
+++ marathon-autoscale.py
@@ -6,28 +6,30 @@ import json
 import math
 import time
 
-dcos_master = input("Enter the DNS hostname or IP of your Marathon Instance : ")
+#dcos_master = input("Enter the DNS hostname or IP of your Marathon Instance : ")
 #max_mem_percent = int(input("Enter the Max percent of Mem Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : "))
 #max_cpu_time = int(input("Enter the Max percent of CPU Usage averaged across all Application Instances to trigger Autoscale (ie. 80) : "))
 #trigger_mode = input("Enter which metric(s) to trigger Autoscale ('and', 'or') : ")
 #autoscale_multiplier = float(input("Enter Autoscale multiplier for triggered Autoscale (ie 1.5) : "))
 #max_instances = int(input("Enter the Max instances that should ever exist for this application (ie. 20) : "))
-userid = input('Enter the username for the DCOS cluster : ')
-password = input('Enter the password for the DCOS cluster : ')
-marathon_app = input("Enter the Marathon Application Name to Configure Autoscale for from the Marathon UI : ")
+#userid = input('Enter the username for the DCOS cluster : ')
+#password = input('Enter the password for the DCOS cluster : ')
+#marathon_app = input("Enter the Marathon Application Name to Configure Autoscale for from the Marathon UI : ")
 
-max_mem_percent = 40
-max_cpu_time = 40
-trigger_mode = 'or'
-max_instances = 10
-autoscale_multiplier=4
-
+#max_mem_percent = 40
+#max_cpu_time = 40
+#trigger_mode = 'or'
+#max_instances = 10
+#autoscale_multiplier=4
 
 class Marathon(object):
     def __init__(self, dcos_master,dcos_auth_token):
         self.name = dcos_master
         self.uri=(dcos_master)
-        self.headers={'Authorization': 'token='+dcos_auth_token, 'Content-type': 'application/json'}
+        if dcos_auth_token is not None:
+            self.headers={'Authorization': 'token='+dcos_auth_token, 'Content-type': 'application/json'}
+        else:
+            self.headers={'Content-type': 'application/json'}
         self.apps = self.get_all_apps()
 
     def get_all_apps(self):
@@ -74,6 +76,20 @@ class Marathon(object):
         response=requests.put(self.uri + '/service/marathon/v2/apps/'+ marathon_app, json_data,headers=self.headers,verify=False)
         print ('Scale_app return status code =', response.status_code)
 
+    def scale_down_app(self, marathon_app, autoscale_multiplier):
+        target_instances=math.ceil(self.appinstances / autoscale_multiplier)
+        if (target_instances < min_instances):
+            print("No scale, reached the minimum instances of ", min_instances)
+            target_instances=min_instances
+        else:
+            target_instances=target_instances
+        if (self.appinstances != target_instances):
+            data ={'instances': target_instances}
+            json_data=json.dumps(data)
+            #headers = {'Content-type': 'application/json'}
+            response=requests.put(self.uri + '/service/marathon/v2/apps/'+ marathon_app, json_data,headers=self.headers,verify=False)
+            print ('Scale_down_app return status code =', response.status_code)
+
 def dcos_auth_login(dcos_master,userid,password):
     '''
     Will login to the DCOS ACS Service and RETURN A JWT TOKEN for subsequent API requests to Marathon, Mesos, etc
@@ -88,7 +104,10 @@ def get_task_agentstatistics(task, agent):
     # Get the performance Metrics for all the tasks for the Marathon App specified
     # by connecting to the Mesos Agent and then making a REST call against Mesos statistics
     # Return to Statistics for the specific task for the marathon_app
-    dcos_headers={'Authorization': 'token='+dcos_auth_token, 'Content-type': 'application/json'}
+    if dcos_auth_token is None:
+        dcos_headers={'Content-type': 'application/json'}
+    else:
+        dcos_headers={'Authorization': 'token='+dcos_auth_token, 'Content-type': 'application/json'}
     response = requests.get(dcos_master + '/slave/' + agent + '/monitor/statistics.json', verify=False, headers=dcos_headers, allow_redirects=True).json()
     # print ('DEBUG -- Getting Mesos Metrics for Mesos Agent =',agent)
     for i in response:
@@ -104,11 +123,50 @@ def timer():
     return
 
 if __name__ == "__main__":
-    print ("This application tested with Python3 only")
-    dcos_auth_token=dcos_auth_login(dcos_master,userid,password)
-    print('Auth Token is = ' + dcos_auth_token)
+    import argparse
 
+    print ("This application tested with Python3 only")
+    parser = argparse.ArgumentParser(description='Marathon autoscale app.')
+    parser.add_argument('--dcos-master', help='The DNS hostname or IP of your Marathon Instance', required=True)
+    parser.add_argument('--max_mem_percent', help='The Max percent of Mem Usage averaged across all Application Instances to trigger Autoscale (ie. 80)', required=True, type=float)
+    parser.add_argument('--max_cpu_time', help='The Max percent of CPU Usage averaged across all Application Instances to trigger Autoscale (ie. 80)', required=True, type=float)
+    parser.add_argument('--trigger_mode', help='Which metric(s) to trigger Autoscale ('and', 'or')', required=True)
+    parser.add_argument('--autoscale_multiplier', help='Autoscale multiplier for triggered Autoscale (ie 1.5)', required=True, type=float)
+    parser.add_argument('--max_instances', help='The Max instances that should ever exist for this application (ie. 20)', required=True, type=int)
+    parser.add_argument('--userid', help='Username for the DCOS cluster')
+    parser.add_argument('--password', help='Password for the DCOS cluster')
+    parser.add_argument('--marathon-app', help='Marathon Application Name to Configure Autoscale for from the Marathon UI', required=True)
+    parser.add_argument('--min_instances', help='Minimum number of instances to maintain', required=True, type=int)
+    parser.add_argument('--cool-down-factor', help='Number of cycles to avoid scaling again', required=True, type=int)
+    parser.add_argument('--trigger_number', help='Number of cycles to avoid scaling again', required=True, type=int)
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        parser.print_help()
+        sys.exit(1)
+
+    dcos_master = args.dcos_master
+    max_mem_percent = float(args.max_mem_percent)
+    max_cpu_time = float(args.max_cpu_time)
+    trigger_mode = args.trigger_mode
+    autoscale_multiplier = float(args.autoscale_multiplier)
+    max_instances = float(args.max_instances)
+    userid = args.userid
+    password = args.password
+    marathon_app = args.marathon_app
+    min_instances = float(args.min_instances)
+    cool_down_factor = float(args.cool_down_factor)
+    trigger_number = float(args.trigger_number)
+
+    if userid is not None:
+        dcos_auth_token=dcos_auth_login(dcos_master,userid,password)
+        print('Auth Token is = ' + dcos_auth_token)
+    else:
+        dcos_auth_token=None
     running=1
+    #Initialize variables
+    cool_down=0
+    trigger_var = 0
     while running == 1:
         # Initialize the Marathon object
         aws_marathon = Marathon(dcos_master,dcos_auth_token)
@@ -123,7 +181,8 @@ if __name__ == "__main__":
             print ("  Found your Marathon App=", marathon_app)
         else:
             print ("  Could not find your App =", marathon_app)
-            sys.exit(1)
+            timer()
+            continue
         # Return a dictionary comprised of the target app taskId and hostId.
         app_task_dict = aws_marathon.get_app_details(marathon_app)
         print ("    Marathon  App 'tasks' for", marathon_app, "are=", app_task_dict)
@@ -176,15 +235,41 @@ if __name__ == "__main__":
         #Evaluate whether an autoscale trigger is called for
         print('\n')
         if (trigger_mode == "and"):
-            if (app_avg_cpu > max_cpu_time) and (app_avg_mem > max_mem_percent):
+            if (app_avg_cpu > max_cpu_time) and (app_avg_mem > max_mem_percent) and (trigger_var >= trigger_number):
                 print ("Autoscale triggered based on 'both' Mem & CPU exceeding threshold")
                 aws_marathon.scale_app(marathon_app, autoscale_multiplier)
+                trigger_var = 0
+            elif (app_avg_cpu < max_cpu_time) and (app_avg_mem < max_mem_percent) and (cool_down >= cool_down_factor):
+                print ("Autoscale down triggered based on 'both' Mem & CPU are down the threshold")
+                aws_marathon.scale_down_app(marathon_app, autoscale_multiplier)
+                cool_down = 0
+            elif (app_avg_cpu > max_cpu_time) and (app_avg_mem > max_mem_percent):
+                trigger_var += 1
+                cool_down = 0
+                print ("Limits exceeded but waiting for trigger_number to be exceeded too to scale up, ", trigger_var)
+            elif (app_avg_cpu < max_cpu_time) and (app_avg_mem < max_mem_percent) and (cool_down < cool_down_factor):
+                cool_down += 1
+                trigger_var = 0
+                print ("Limits are not exceeded but waiting for trigger_number to be exceeded too to scale down, ", cool_down)
             else:
                 print ("Both values were not greater than autoscale targets")
         elif (trigger_mode == "or"):
-            if (app_avg_cpu > max_cpu_time) or (app_avg_mem > max_mem_percent):
+            if ((app_avg_cpu > max_cpu_time) or (app_avg_mem > max_mem_percent)) and (trigger_var >= trigger_number):
                 print ("Autoscale triggered based Mem 'or' CPU exceeding threshold")
                 aws_marathon.scale_app(marathon_app, autoscale_multiplier)
+                trigger_var = 0
+            elif ((app_avg_cpu < max_cpu_time) or (app_avg_mem < max_mem_percent)) and (cool_down >= cool_down_factor):
+                print ("Autoscale triggered based on Mem or CPU are down the threshold")
+                aws_marathon.scale_down_app(marathon_app, autoscale_multiplier)
+                cool_down = 0
+            elif (app_avg_cpu > max_cpu_time) or (app_avg_mem > max_mem_percent):
+                trigger_var += 1
+                cool_down = 0
+                print ("Limits exceeded but waiting for trigger_number to be exceeded too to scale up, ", trigger_var)
+            elif (app_avg_cpu < max_cpu_time) or (app_avg_mem < max_mem_percent):
+                cool_down += 1
+                trigger_var = 0
+                print ("Limits are not exceeded but waiting for trigger_number to be exceeded too to scale down, ", cool_down)
             else:
                 print ("Neither Mem 'or' CPU values exceeding threshold")
         timer()
